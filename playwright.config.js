bde5ffa99efd92a52c662449830b4ec32a529306
Eugene Petrik <eugeniy.petrik@gmail.com>
2024-11-15 16:15:06 +0200

    Update project dependencies

--- playwright.config.js
+++ playwright.config.js
@@ -22,8 +22,8 @@ module.exports = defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
 
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    /* Opt out of parallel tests. */
+    workers: '75%',
 
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
